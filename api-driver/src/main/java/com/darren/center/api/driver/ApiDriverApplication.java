af6a36d589f7a712d63832eb950427d77188dd76
Darren <1240101543@qq.com>
2020-08-12 22:09:18 +0800

    Ribbon独立使用

--- api-driver/src/main/java/com/darren/center/api/driver/ApiDriverApplication.java
+++ api-driver/src/main/java/com/darren/center/api/driver/ApiDriverApplication.java
@@ -3,11 +3,10 @@ package com.darren.center.api.driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class ApiDriverApplication {
 
