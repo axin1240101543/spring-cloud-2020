82a03ea9c6d5d64de87734df7bcf0484a6c4aee1
Darren <1240101543@qq.com>
2020-08-18 14:43:57 +0800

    feign中捕获熔断的异常信息

--- api-passenger/src/main/java/com/darren/center/api/passenger/feign/SmsClient.java
+++ api-passenger/src/main/java/com/darren/center/api/passenger/feign/SmsClient.java
@@ -2,7 +2,8 @@ package com.darren.center.api.passenger.feign;
 
 import com.darren.center.service.common.dto.ResponseResult;
 import com.darren.center.service.common.dto.sms.SmsSendRequest;
-import com.darren.center.service.common.enums.CommonStatusEnum;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author : Darren
  * @date : 2020年08月12日 14:40:32
  **/
-@FeignClient(name = "service-sms", fallback = SmsClient.SmsClientFallback.class)
+//@FeignClient(name = "service-sms", fallback = SmsClient.SmsClientFallback.class)
+@FeignClient(name = "service-sms", fallbackFactory = SmsClient.SmsClientFallbackFactory.class)
 public interface SmsClient {
 
     /**
@@ -32,7 +34,23 @@ public interface SmsClient {
     class SmsClientFallback implements SmsClient{
         @Override
         public ResponseResult sendSms(SmsSendRequest smsSendRequest) throws Exception {
-            return ResponseResult.fail(-3, "feign熔断");
+            return ResponseResult.fail(-3, "feign fallback熔断");
+        }
+    }
+
+    @Component
+    @Slf4j
+    class SmsClientFallbackFactory implements FallbackFactory<SmsClient> {
+
+        @Override
+        public SmsClient create(Throwable throwable) {
+            return new SmsClient() {
+                @Override
+                public ResponseResult sendSms(SmsSendRequest smsSendRequest) throws Exception {
+                    log.error("异常信息:", throwable);
+                    return ResponseResult.fail(-3, "feign fallbackFactory熔断");
+                }
+            };
         }
     }
 }
