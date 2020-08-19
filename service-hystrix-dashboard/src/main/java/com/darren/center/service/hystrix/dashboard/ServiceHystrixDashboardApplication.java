f82ffe56005d5bb65943ef0343bbf5941bd07ab4
Darren <1240101543@qq.com>
2020-08-19 10:37:11 +0800

    hystrix dashboard对多个服务可视化（turbine）

--- service-hystrix-dashboard/src/main/java/com/darren/center/service/hystrix/dashboard/ServiceHystrixDashboardApplication.java
+++ service-hystrix-dashboard/src/main/java/com/darren/center/service/hystrix/dashboard/ServiceHystrixDashboardApplication.java
@@ -2,15 +2,26 @@ package com.darren.center.service.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
- * http://localhost:8888/hystrix
+ * http://localhost:8888/hystrix  #监控面板
  *
- * http://localhost:8080/actuator/hystrix.stream
+ * 前提：开启Admin监控
+ * http://localhost:8080/actuator/hystrix.stream、http://localhost:8081/actuator/hystrix.stream #只能监控一个服务，实际生产中不方便。
+ * http://localhost:8888/turbine.stream #也是一直ping，相当于原来的hystrix.stream,不过此处是综合了所有的项目
+ *
+ * Turbine的坑：
+ * https://www.cnblogs.com/houzheng/p/9906800.html
  */
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableEurekaClient //表示此工程可以向注册中心提供服务
+@EnableHystrix //表示开启熔断器
+@EnableHystrixDashboard //表示开启熔断监控
+@EnableTurbine //表示开启监控数据聚合
 public class ServiceHystrixDashboardApplication {
 
     public static void main(String[] args) {
