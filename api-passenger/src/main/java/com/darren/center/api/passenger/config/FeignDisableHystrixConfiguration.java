62e5ba6da546b4f022bf3c8b1c6643a76e51c9fa
Darren <1240101543@qq.com>
2020-08-19 12:25:16 +0800

    feign中设置hystrix的参数

--- api-passenger/src/main/java/com/darren/center/api/passenger/config/FeignDisableHystrixConfiguration.java
+++ api-passenger/src/main/java/com/darren/center/api/passenger/config/FeignDisableHystrixConfiguration.java
@@ -14,14 +14,24 @@ import org.springframework.context.annotation.Scope;
  * @author : Darren
  * @date : 2020年08月18日 15:23:45
  **/
-@Configuration
 @ExcludeFeignConfig
+@Configuration
 public class FeignDisableHystrixConfiguration {
 
     @Bean
     @Scope("prototype")
     public Feign.Builder feignBuilder(){
+        //禁用feign客户端的hystrix
         return Feign.builder();
+        //给feign配置hystrix的属性 https://blog.csdn.net/weixin_33788244/article/details/86013793
+        /*return HystrixFeign.builder().setterFactory(new SetterFactory() {
+            @Override
+            public HystrixCommand.Setter create(Target<?> target, Method method) {
+                return HystrixCommand.Setter
+                        .withGroupKey(HystrixCommandGroupKey.Factory.asKey(SmsClient.class.getSimpleName()))// 控制ShortMsgService下,所有方法的Hystrix Configuration
+                        .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter().withCoreSize(200));//配置线程池大小,默认值10个
+            }
+        });*/
     }
 
 }
