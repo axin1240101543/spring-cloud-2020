778b0510dac1eda71f15e0cf7cefe80fc4190e3a
Darren <1240101543@qq.com>
2020-08-19 14:27:35 +0800

    zuul配置指定微服务的访问路径

--- zuul/src/main/java/com/darren/center/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/darren/center/zuul/ZuulApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 /**
  * 该注解声明这是一个zuul代理，该代理使用Ribbon来定位注册到eureka server上的微服务，同时，整合了hystrix，实现了容错。
  *
+ * zuul默认集成了：ribbon和hystrix。
+ *
  * http://网关ip:网关端口/服务名/微服务路径
  * http://localhost:9000/api-driver/test/hello?name=darren&age=18  相当于访问 http://localhost:8080/test/hello?name=darren&age=18
  */
