838a6778f44077564b922fc5751ab47ca952fc87
Darren <1240101543@qq.com>
2020-08-19 14:04:56 +0800

    zuul

---
+++ api-driver/src/main/java/com/darren/center/api/driver/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.darren.center.api.driver.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * <h3>spring-cloud-2020</h3>
+ * <p></p>
+ *
+ * @author : Darren
+ * @date : 2020年08月19日 13:49:51
+ **/
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    /**
+     * 测试zuul
+     * @param name
+     * @param age
+     * @return
+     */
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name, @RequestParam  Integer age){
+        log.info("name:{}, age:{}", name, age);
+        String[] str = new String[]{name, age.toString()};
+        return Arrays.toString(str);
+    }
+
+}
